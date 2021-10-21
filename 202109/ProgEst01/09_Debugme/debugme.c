c4a24e1c8c17c2164fc95e7882083b065af6bee8
sotrteacher <lamberto9@gmail.com>
2021-10-21 13:27:38 -0500

    2021.10.21.13.27

---
+++ 202109/ProgEst01/09_Debugme/debugme.c
@@ -0,0 +1,28 @@
+/*
+ * debugme.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#define BIGNUM	5000
+
+void index_to_the_moon(int ary[]);
+
+int main(void)
+{
+  int intary[100];
+  index_to_the_moon(intary);
+  exit(EXIT_SUCCESS);
+}
+
+void index_to_the_moon(int ary[])
+{
+  int i;
+ #if 0
+  for(i = 0; i < BIGNUM; ++i)
+ #else
+  for(i = 0; i < 100; ++i)
+ #endif
+    ary[i] = i;
+}
+
+

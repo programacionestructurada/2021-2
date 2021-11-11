918d5d19e0b60b5819dadcee52e015bd1769f7c4
usuario <usuario@2806-105e-0017-1459-6600-6aff-fefb-ce31.ipv6.infinitum.net.mx>
2021-11-11 15:08:31 -0600

    2021.11.11.15.08

---
+++ 202110/ProgEst02/00_04_SumaDVectores/SumaDVectores.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 100
+//#define USE_ARRAYS
+#define SIZE_ARR(arr)   (sizeof(arr)/sizeof(arr[0]))
+
+void print_array(float A[],unsigned int tam){
+ int i;
+ for(i=1;i<=tam;i++){
+  printf("%4.2f\t",A[i]);
+ }
+}
+
+int main(){
+ int I,N;
+#ifdef USE_ARRAYS
+ float A[TAM],B[TAM],C[TAM];
+#else
+ float *A,*B,*C;
+#endif // 1
+ printf("PROGRAMA QUE REALIZA UNA SUMA DE VECTORES\n");
+ printf("Introduce el valor de N: ");
+ scanf("%d",&N);
+
+#ifdef USE_ARRAYS
+ /** ENTRADA */
+ for (I=1;!(I>N);I++) {
+   printf("A[%i] = ",I);
+   scanf("%f",&A[I]);
+   printf("B[%i] = ",I);
+   scanf("%f",&B[I]);
+ }
+ #if 1
+ printf("\n");
+ print_array(A,N);
+ printf("\n");
+ print_array(B,N);
+ #endif
+  /** PROCESO */
+  for (I=1;!(I>N);I++) {
+    C[I]=A[I]+B[I];
+  }
+  /** SALIDA */
+  printf("\n");
+  for (I=1;!(I>N);I++) {
+    printf("%6.2f\t",C[I]);
+  }
+#else
+ A=(float *)malloc((N+1)*sizeof(float));
+ B=(float *)malloc((N+1)*sizeof(float));
+ C=(float *)malloc((N+1)*sizeof(float));
+ /** ENTRADA */
+ for (I=1;!(I>N);I++) {
+   printf("A[%i] = ",I);
+   scanf("%f",&A[I]);
+   printf("B[%i] = ",I);
+   scanf("%f",&B[I]);
+ }
+ #if 1
+ printf("\n");
+ print_array(A,N);
+ printf("\n");
+ print_array(B,N);
+ #endif
+  /** PROCESO */
+  for (I=1;!(I>N);I++) {
+    C[I]=A[I]+B[I];
+  }
+  /** SALIDA */
+  printf("\n");
+  for (I=1;!(I>N);I++) {
+   #if 0
+    printf("%6.2f\t",C[I]);
+   #else
+    printf("%6.2f\t",*(C+I));
+   #endif // 0
+  }
+  free(A);
+  free(B);
+  free(C);
+#endif // USE_ARRAYS
+
+
+ return 0;
+}/*end main()*/
+

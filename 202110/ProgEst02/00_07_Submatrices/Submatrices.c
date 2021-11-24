52ef5370daca26302fa5f23a547ed28b8c3ee828
usuario <usuario@2806-105e-0017-1459-6600-6aff-fefb-ce31.ipv6.infinitum.net.mx>
2021-11-24 12:53:56 -0600

    2021.11.24.12.53

--- 202110/ProgEst02/00_07_Submatrices/Submatrices.c
+++ 202110/ProgEst02/00_07_Submatrices/Submatrices.c
@@ -19,41 +19,44 @@ struct matriz{
 int P, Q, i, j, k, z;
 float** PQ;
 
-float** sub_matrix_(struct matriz*M, int colAElim)
+float** sub_matrix_(struct matriz *M,int colAElim)
 {
       int i,j;
       float** R=(float**)malloc((M->m-1)*sizeof(float*));
-      for(i=0;i<M->m-1;++i)
+      for(i=0;i < M->m-1;++i)
         R[i]=(float*)malloc((M->m-1)*sizeof(float));
-      for(i=1;i<M->m;++i)
-        for(j=0;j<M->n;++j){
+      for(i=1;i < M->m;++i)
+        for(j=0;j < M->n;++j){
           if(j<colAElim){
-            R[i][j]=M->A[i][j];
+            R[i-1][j]=M->A[i][j];
           }
           if(j>colAElim){
-            R[i][j-1]=M->A[i][j];
+            R[i-1][j-1]=M->A[i][j];
         }
      }
      return R;
 }
 
-void print_matriz(struct matriz*M){
+void print_matriz(struct matriz *M){
      int i,j;
-     for(i=0;i<M->m;++i){
-       for(j=0;j<M->n;++j){
-         printf("%.2f\t",M->A[i][j]);
+     for(i=0;i < M->m;++i){
+       for(j=0;j < M->n;++j){
+         printf("%8.2f",M->A[i][j]);
        }
        printf("\n");
      }
 }
 
+/** Copia la matriz a la que apunta from en la estructura a la
+ *  que apunta to
+ */
 void copy_matriz(struct matriz *to,struct matriz *from){
     int i,j;
     to->m = from->m;
     to->n = from->n;
     to->A=(float**)malloc(from->m*sizeof(float*));
     for(i=0;i < from->m;++i)
-      F[m]=(float*)malloc((from->n)*sizeof(float));
+      to->A[i] = (float*)malloc((from->n)*sizeof(float));
     for(i=0;i<from->m;++i){
        for(j=0;j<from->n;++j){
          to->A[i][j] = from->A[i][j];
@@ -63,6 +66,7 @@ void copy_matriz(struct matriz *to,struct matriz *from){
 
 int main (void)
 {
+#if 0 //LMC 2021.11.24
     float*p=(float* )malloc(2*sizeof (float));
     *p=40.41;
     p[1]=42.43;
@@ -77,7 +81,7 @@ int main (void)
     //float**=(float**)malloc(2*sizeof(float*));
     PQ[0]=p;
     PQ[1]=q;
-  #if 1
+  #if 0
     //float** PQ;//matriz de mxn
     int i, j, m=2,n=2;
     for(i=0;i<m;++i){
@@ -93,9 +97,52 @@ int main (void)
     SM.A=PQ;
     print_matriz(&SM);
   #endif
+#endif
+    struct matriz SM;
+    printf("Prueba de la funci\\'on \
+float** sub_matrix_(struct matriz *M,int colAElim)\n");
+    struct matriz M;
+    M.m = M.n = 3;
 
+    M.A = (float **)malloc(3*sizeof(float *));
+    M.A[0] = (float *)malloc(3*sizeof(float));
+    M.A[1] = (float *)malloc(3*sizeof(float));
+    M.A[2] = (float *)malloc(3*sizeof(float));
+    M.A[0][0]=1;M.A[0][1]=2;M.A[0][2]=3;
+    M.A[1][0]=4;M.A[1][1]=40.41;M.A[1][2]=42.43;
+    M.A[2][0]=5;M.A[2][1]=44.45;M.A[2][2]=46.47;
+    printf("\n");
+    print_matriz(&M);
+    printf("\n");
 
+    float **sm = sub_matrix_(&M,0);
+    SM.A = sm;
+    SM.m=SM.n=2;
+    printf("Submatriz que se obtiene eliminando la fila 0 \
+y la columna 0:\n");
+    print_matriz(&SM);
+    printf("\n");
+    free(sm);
+    sm = sub_matrix_(&M,1);
+    SM.A = sm;
+    printf("Submatriz que se obtiene eliminando la fila 0 \
+y la columna 1:\n");
+    print_matriz(&SM);
+    printf("\n");
+    free(sm);
+    sm = sub_matrix_(&M,2);
+    SM.A = sm;
+    printf("Submatriz que se obtiene eliminando la fila 0 \
+y la columna 2:\n");
+    print_matriz(&SM);
 
+    free(sm);
+    free(M.A);
+#if 0 //LMC 2021.11.24
+    free(p);
+    free(q);
+    free(PQ);
+#endif // 2021.11.24
     return 0;
 }/*end main()*/
 

55fcdd059b145dcaee88ae3e3ce0237dbcd828cf
usuario <usuario@2806-105e-0017-1459-6600-6aff-fefb-ce31.ipv6.infinitum.net.mx>
2022-06-06 05:11:50 -0500

    2022.06.06.05.12

--- IntroProgUnidad02/IntroProgUnidad02_util.h
+++ IntroProgUnidad02/IntroProgUnidad02_util.h
@@ -10,5 +10,8 @@ void set_formato(char formato[],struct ConjDInts CdI);
 void print_like_table(short rows,struct ConjDInts CdI,char (**two_dim_char_array_pt)[][COLUMN_W]);
 void create_h_rule(char h_rule[SHRT_MAX],struct ConjDInts cdi);
 //typedef char (*two_dim_char_array_pt_t)[][COLUMN_W];
+/**
+ * @param t_c: two dimension char array pointer type table content.
+ */
 void tabla(short rows,short cols,two_dim_char_array_pt_t t_c[]);
 #endif // INTROPROGUNIDAD02_UTIL_INCLUDED
